69d14b3e002b8aee21c2f7f50b373d04ef2976cb
faheem4531 <faheem4531@gmail.com>
2024-08-23 04:03:30 +0500

    color picker fixes

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,7 +1,11 @@
-import React from "react";
 import { Html, useProgress } from "@react-three/drei";
+import React from "react";
 
 export default function Loader() {
   const { progress } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
+  return (
+    <Html center>
+      <span style={{ color: "#c2c2c2" }}>{progress} % loaded</span>
+    </Html>
+  );
 }
