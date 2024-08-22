1c9f17c972f9bdd1db5fb11d61cbdb3282c51409
faheem4531 <faheem4531@gmail.com>
2024-08-22 23:49:38 +0500

    showing colors value, and get color by value

--- src/Components/ColorPicker.jsx
+++ src/Components/ColorPicker.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import { HexColorPicker } from "react-colorful";
 import { useSnapshot } from "valtio";
-
 export default function ColorPicker(props) {
   const snap = useSnapshot(props.state);
+
+  const [color, setColor] = useState(snap.colors[snap.current]);
+
+  const handleColorChange = (newColor) => {
+    setColor(newColor);
+    props.updateColor(snap.current, newColor);
+  };
+
+  const handleInputChange = (event) => {
+    const inputColor = event.target.value;
+    setColor(inputColor);
+    props.updateColor(snap.current, inputColor);
+  };
+
   return (
     <div
       className={snap.current !== null ? "color-picker" : "color-picker hidden"}
     >
-      <HexColorPicker
-        color={snap.colors[snap.current]}
-        onChange={(color) => props.updateColor(snap.current, color)}
+      <HexColorPicker color={color} onChange={handleColorChange} />
+      <input
+        type="text"
+        value={color}
+        onChange={handleInputChange}
+        maxLength={7}
+        style={{ marginTop: "10px", padding: "5px", width: "100px" }}
       />
       <h1>{snap.current}</h1>
     </div>
